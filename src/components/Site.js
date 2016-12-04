a0c6acfc4f4d6c8a16e91f4e05438be368b0128a
Deepak Jois <deepak.jois@gmail.com>
2016-12-04 22:27:17 +0530

    Add fadeIn animation

--- src/components/Site.js
+++ src/components/Site.js
@@ -7,9 +7,18 @@ import FAQ from './FAQ'
 import Contact from './Contact'
 import Footer from './Footer'
 
+let fadeIn = css.keyframes({
+  '0%': { opacity: 0},
+  '100%': { opacity: 1}
+})
+
+let animation = css({
+  animation: `${fadeIn} 2.0s ease forwards`
+})
+
 export default function Site () {
   return (
-    <div className="container">
+    <div className="container" {...animation}>
       <div className="row"><Nav/></div>
       <div className="row"><Header/></div>
       <div className="row"><HowItWorks/></div>
