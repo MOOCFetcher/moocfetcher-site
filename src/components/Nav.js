311245a5b474e03dddeab6c748fe4d148d54df5d
Deepak Jois <deepak.jois@gmail.com>
2016-12-05 21:11:17 +0530

    Styling and links for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,37 @@
 import React from 'react'
-import { css } from 'glamor'
+import { css, select as $ } from 'glamor'
 
-let block = css({
+let nav = css({
   display: 'block',
   position: 'fixed',
+  background: '#fff',
   top: 0,
-  left: 0
+  left: 0,
+  width: '100%'
+},
+$('& ul', {
+  listStyle: 'none',
+  margin: 0
+}),
+$( '& ul > li', {
+  display: 'inline-block',
+  padding: '0 1rem',
+  fontSize: '2.4rem'
+}),
+$('& ul > li.active a',{
+  color: '#606c76'
 })
+)
 
 export default function Nav () {
   return (
-    <nav {...block}>
-      <h3> Nav Goes Here</h3>
+    <nav {...nav}>
+      <ul>
+        <li className="active"><a href="#top">Home</a></li>
+        <li><a href="#how-it-works">How It Works</a></li>
+        <li><a href="#faq">FAQs</a></li>
+        <li><a href="#contact">Contact Us</a></li>
+      </ul>
     </nav>
   )
 }
